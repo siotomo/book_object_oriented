03b4af6bf639bd5fc03f41943a93ee5efecf524e
shiomi <shiomi@glomouth.co.jp>
2023-03-12 14:09:00 +0900

    gear_inch実装後、既存コードが壊れる

--- gear.rb
+++ gear.rb
@@ -1,18 +1,25 @@
 # frozen_string_literal: true
 
 class Gear
-  attr_accessor :chainring, :cog
+  attr_accessor :chainring, :cog, :rim, :tire
 
-  def initialize(chainring, cog)
+  def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
 
   def ratio
     chainring / cog.to_f
   end
+
+  def gear_inch
+    ratio * (rim + tire * 2)
+  end
 end
 
-gear = Gear.new(51, 11)
+gear = Gear.new(51, 11) # error
+gear = Gear.new(51, 11, 2, 10)
 
-p gear.ratio
+p gear.gear_inch
